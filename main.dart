923d74a211c27039fdf285553f73d4dee5cab6f8
Rerison-Alves <rerison.alves07@aluno.ifce.edu.br>
2022-10-14 15:14:12 -0300

    Imprime expressão original

--- main.dart
+++ main.dart
@@ -1,5 +1,51 @@
 import 'dart:io';
 
 void main(){
-  print('Hello World');
+  print('Digite a expressão:');
+  var expressao = stdin.readLineSync();
+  Infix_to_postfix infix_to_postfix = new Infix_to_postfix();
+
+  print(infix_to_postfix.convert(expressao));
+
+}
+
+class Infix_to_postfix{
+
+  List<String> pilha = List.filled(200, ''), operadores = List.filled(200, '');
+  int toppilha = 0, topoperadores = 0;
+
+  String convert(var expressao){
+  for (var x in expressao.split(' ')) {
+    pushpilha(x, pilha);
+  }
+  var expressaofinal = '';
+  for(String x in pilha){
+    expressaofinal+= x + ' ';
+  }
+  return expressaofinal;
+  }
+
+  void pushpilha(String i, List<String> pilha) {
+    if (pilha[0] != 1 / 0) toppilha++;
+    pilha[toppilha] = i;
+  }
+
+  String pop(List<String> pilha) {
+    String i = pilha[toppilha];
+    pilha[toppilha] = '';
+    if (toppilha != 1 / 0) toppilha--;
+    return i;
+  }
+
+  void pushOperacoes(String i, List<String> pilha) {
+    if (pilha[0] != 1 / 0) topoperadores++;
+    pilha[topoperadores] = i;
+  }
+
+  String popOperacoes(List<String> pilha) {
+    String i = pilha[topoperadores];
+    pilha[topoperadores] = '';
+    if (topoperadores != 1 / 0) topoperadores--;
+    return i;
+  }
 }
