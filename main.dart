663bb96f5e15cd8b05e05bf2544e3462f999b33f
Israel Marques de Oliveira <90154937+IsraelMarqs@users.noreply.github.com>
2022-10-20 17:34:21 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -10,57 +10,66 @@ void main(){
 }
 
 class Infix_to_postfix{
-	RegExp _numeric = RegExp(r'^-?[0-9]+$');
+  RegExp regex = RegExp(r'^-?[0-9]+$');
   List<String> pilha = List.filled(200, ''), operadores = List.filled(200, '');
   int toppilha = 0, topoperadores = 0;
 
-  String convert(var expressao){
-  for (var x in expressao.split(' ')) {
-    pushpilha(x, pilha);
-		
-  }
-  var expressaofinal = '';
-  for(String x in pilha){
-    expressaofinal+= x + ' ';
-  }
-  return expressaofinal;
-  }
-
-  void pushpilha(String i, List<String> pilha) {
-    if (pilha[0] != 1 / 0) toppilha++;
-    pilha[toppilha] = i;
-  }
-
-  String pop(List<String> pilha) {
-    String i = pilha[toppilha];
-    pilha[toppilha] = '';
-    if (toppilha != 1 / 0) toppilha--;
-    return i;
-  }
-
-  void pushOperacoes(String i, List<String> pilha) {
-    if (pilha[0] != 1 / 0) topoperadores++;
-    pilha[topoperadores] = i;
-  }
-
-  String popOperacoes(List<String> pilha) {
-    String i = pilha[topoperadores];
-    pilha[topoperadores] = '';
-    if (topoperadores != 1 / 0) topoperadores--;
-    return i;
-  }
-	int preced(ch) {
-   if(ch == '+' || ch == '-') {
-      return 1;             
-   }else if(ch == '*' || ch == '/') {
-      return 2;            
-   }else if(ch == '^') {
-      return 3;            
-   }else {
+  int preced(x) {
+    if (x == '+' || x == '-') {
+      return 1;
+    } else if (x == '*' || x == '/') {
+      return 2;
+    } else {
       return 0;
-   }
-		bool isNumeric(String str) {
-return _numeric.hasMatch(str);
-}
-}
-}
+    }
+  }
+
+    String convert(var expressao){
+      for (var x in expressao.split(' ')) {
+        if (x == '+' && x != '-' && x != '*' && x != '/') {
+        pushOperacoes(x, pilha);
+        }
+        if (regex.hasMatch(x)) {
+        pushpilha(x, pilha);
+        }
+        else {
+          print("Invalido");
+        }
+
+        pushpilha(x, pilha);
+
+      }
+      var expressaofinal = '';
+      for(String x in pilha){
+        expressaofinal+= x + ' ';
+      }
+
+      return expressaofinal;
+    }
+
+    void pushpilha(String i, List<String> pilha) {
+      if (pilha[0] != 1 / 0) toppilha++;
+      pilha[toppilha] = i;
+    }
+
+    String pop(List<String> pilha) {
+      String i = pilha[toppilha];
+      pilha[toppilha] = '';
+      if (toppilha != 1 / 0) toppilha--;
+      return i;
+    }
+
+    void pushOperacoes(String i, List<String> pilha) {
+      if (pilha[0] != 1 / 0) topoperadores++;
+      pilha[topoperadores] = i;
+    }
+
+    String popOperacoes(List<String> pilha) {
+      String i = pilha[topoperadores];
+      pilha[topoperadores] = '';
+      if (topoperadores != 1 / 0) topoperadores--;
+      return i;
+    }
+
+
+  }
