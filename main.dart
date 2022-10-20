ec3281e09891092a59b7e62037766802c3a3159e
IsraelMarqs <israelmarques007@gmail.com>
2022-10-20 17:35:08 -0300

    mod

--- main.dart
+++ main.dart
@@ -10,42 +10,66 @@ void main(){
 }
 
 class Infix_to_postfix{
-
+  RegExp regex = RegExp(r'^-?[0-9]+$');
   List<String> pilha = List.filled(200, ''), operadores = List.filled(200, '');
   int toppilha = 0, topoperadores = 0;
 
-  String convert(var expressao){
-  for (var x in expressao.split(' ')) {
-    pushpilha(x, pilha);
-  }
-  var expressaofinal = '';
-  for(String x in pilha){
-    expressaofinal+= x + ' ';
-  }
-  return expressaofinal;
-  }
-
-  void pushpilha(String i, List<String> pilha) {
-    if (pilha[0] != 1 / 0) toppilha++;
-    pilha[toppilha] = i;
+  int preced(x) {
+    if (x == '+' || x == '-') {
+      return 1;
+    } else if (x == '*' || x == '/') {
+      return 2;
+    } else {
+      return 0;
+    }
   }
 
-  String pop(List<String> pilha) {
-    String i = pilha[toppilha];
-    pilha[toppilha] = '';
-    if (toppilha != 1 / 0) toppilha--;
-    return i;
-  }
+    String convert(var expressao){
+      for (var x in expressao.split(' ')) {
+        if (x == '+' && x != '-' && x != '*' && x != '/') {
+        pushOperacoes(x, pilha);
+        }
+        if (regex.hasMatch(x)) {
+        pushpilha(x, pilha);
+        }
+        else {
+          print("Invalido");
+        }
 
-  void pushOperacoes(String i, List<String> pilha) {
-    if (pilha[0] != 1 / 0) topoperadores++;
-    pilha[topoperadores] = i;
-  }
+        pushpilha(x, pilha);
 
-  String popOperacoes(List<String> pilha) {
-    String i = pilha[topoperadores];
-    pilha[topoperadores] = '';
-    if (topoperadores != 1 / 0) topoperadores--;
-    return i;
+      }
+      var expressaofinal = '';
+      for(String x in pilha){
+        expressaofinal+= x + ' ';
+      }
+
+      return expressaofinal;
+    }
+
+    void pushpilha(String i, List<String> pilha) {
+      if (pilha[0] != 1 / 0) toppilha++;
+      pilha[toppilha] = i;
+    }
+
+    String pop(List<String> pilha) {
+      String i = pilha[toppilha];
+      pilha[toppilha] = '';
+      if (toppilha != 1 / 0) toppilha--;
+      return i;
+    }
+
+    void pushOperacoes(String i, List<String> pilha) {
+      if (pilha[0] != 1 / 0) topoperadores++;
+      pilha[topoperadores] = i;
+    }
+
+    String popOperacoes(List<String> pilha) {
+      String i = pilha[topoperadores];
+      pilha[topoperadores] = '';
+      if (topoperadores != 1 / 0) topoperadores--;
+      return i;
+    }
+
+
   }
-}
