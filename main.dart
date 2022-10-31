b1fab06dbb49e3b21d2e0415ec24cb1d6848481d
IsraelMarqs <israelmarques007@gmail.com>
2022-10-31 19:18:22 -0300

    erros

--- main.dart
+++ main.dart
@@ -25,27 +25,39 @@ class Infix_to_postfix{
   String convert(var expressao){
     for (var x in expressao.split(' ')) {
       if (x == '+' || x == '-' || x == '*' || x == '/' || x == '(') {
-        if(preced(x, operadores[topoperadores])){
+        if (preced(x, operadores[topoperadores])) {
           pushOperacoes(x, operadores);
-        }else{
-          if(toppilha>0) pushpilha(popOperacoes(operadores), pilha);
+        } else {
+          if (toppilha > 0) pushpilha(popOperacoes(operadores), pilha);
           pushOperacoes(x, operadores);
         }
-      }else {
-        if (x==')'){
+
+      } else {
+        if (x == ')') {
           String i = '';
-          while(i!='('){
+          while (i != '(') {
             i = operadores[topoperadores];
-            if (i=='(') {
+            if (i == '(') {
               popOperacoes(operadores);
-            }else{
-              pushpilha(popOperacoes(operadores), pilha);
             }
-          }
-        }else{
-          pushpilha(x, pilha);
+            }
+
         }
+          if(!regex.hasMatch(x)){
+          print('Expressão invalida');
+          exit(1);
+        }
+
       }
+      if(topoperadores>1){
+        print('ERRO: OPERADOR A MENOS');
+        exit(0);
+      }
+      if (topoperadores < 0) {
+        print('ERRO: OPERADOR A MAIS');
+        exit(0);
+      }
+
       print(pilha);
       print(operadores);
       print(toppilha);
